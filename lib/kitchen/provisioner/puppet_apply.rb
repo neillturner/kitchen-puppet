1faac25033908db5d1da05e974751933d28f7069
Kostyrev Aleksandr <kostyrev@tutu.ru>
2015-05-15 16:57:56 +0300

    fix errors in creation /etc/yum.repos.d/puppettemp.repo

--- lib/kitchen/provisioner/puppet_apply.rb
+++ lib/kitchen/provisioner/puppet_apply.rb
@@ -321,12 +321,13 @@ module Kitchen
           baseurl=http://yum.puppetlabs.com/el/7/dependencies/\$basearch
           gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs
           enabled=0
-          gpgcheck=1' >> /etc/yum.repos.d/puppettemp.repo
+          gpgcheck=1' | sudo tee /etc/yum.repos.d/puppettemp.repo > /dev/null
+          sudo sed -i 's/^[ \t]*//' /etc/yum.repos.d/puppettemp.repo
             #{update_packages_redhat_cmd}
             #{sudo_env('yum')} -y --enablerepo=puppettemp-products --enablerepo=puppettemp-deps install puppet#{puppet_redhat_version}
             # Clean up temporary puppet repo
-            rm -rf /etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs
-            rm -rf /etc/yum.repos.d/puppettemp.repo
+            sudo rm -rf /etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs
+            sudo rm -rf /etc/yum.repos.d/puppettemp.repo
           else
             #{sudo('rpm')} -ivh #{proxy_parm} #{puppet_yum_repo}
             #{update_packages_redhat_cmd}
