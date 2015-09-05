b04b5e8b493cb1d7c750341f177406d7c2853b9f
Neill Turner <neillwturner@gmail.com>
2015-09-05 13:44:10 +0100

    doesn't pass travis checks

--- lib/kitchen/provisioner/puppet_apply.rb
+++ lib/kitchen/provisioner/puppet_apply.rb
@@ -132,7 +132,9 @@ module Kitchen
       default_config :puppet_debug, false
       default_config :puppet_verbose, false
       default_config :puppet_noop, false
-      default_config :platform, provisioner.platform_name
+      default_config :platform  do |provisioner|
+        provisioner.platform_name
+      end  
       default_config :update_package_repos, true
       default_config :remove_puppet_repo, false
       default_config :custom_facts, {}
